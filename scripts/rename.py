185b1da01030d28e0294f7bb20883523584fe8dc
lishuang <qqlishuang@gmail.com>
2021-01-21 14:16:51 +0800

    synchronization

--- scripts/rename.py
+++ scripts/rename.py
@@ -2,29 +2,20 @@
 
 import shutil
 import os
- 
-# new_dir='/home/lishuang/Disk/github/AlphaPose-pytorch/train_sppe/data/coco/images/'
-# ann_dir='/home/lishuang/Disk/shengshi_data/datasets/coco/val2014'
-# i=0
-# for root, _, files in os.walk(ann_dir):
-#     for filename in files:
-#         if filename.endswith('.jpg'):
-#             imagename=filename.replace('COCO_val2014_','')
-#             newdir=os.path.join(new_dir,imagename)
-#             #print(newdir)
-#             if i%100==0:
-#                  print("Processed %d images" % (i))
-#             shutil.copyfile(os.path.join(root,filename),newdir)
-#             i=i+1
-#             #os.rename(root+filename,newdir)
 
-image_dir= '/home/lishuang/Disk/shengshi_data/new_anti_tail/kunming20201102'
-new_dir='/home/lishuang/Disk/shengshi_data/new_anti_tail/kunming20201102_new'
+def check_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+image_dir= '/home/lishuang/Disk/shengshi_data/xilixiang/xingli-20210112/imageset'
+new_dir='/home/lishuang/Disk/shengshi_data/xilixiang/xingli-20210112/JPEGImages'
+check_dir(new_dir)
 i=0
 for root, _, files in os.walk(image_dir):
     for filename in files:
         if filename.endswith('.jpg'):
-            imagename=f"kunming20201102_{i}.jpg"
+            # imagename=f"kunming20201102_{i}.jpg"
+            imagename=filename.replace("-nfs-行李转盘提箱抓拍-天河-威海机场-行李箱检测-行李箱-","")
             newdir=os.path.join(new_dir,imagename)
             #print(newdir)
             if i%100==0:
--- scripts/xml2txt.py
+++ scripts/xml2txt.py
@@ -34,37 +34,50 @@ def check_dir(dir):
 
 
 
-def voc2yolo(filename,wh_thr,ar_thr):
+def voc2yolo(filename,class_name_path,wh_thr,ar_thr):
     classes_dict = {}
-    with open("/home/lishuang/Disk/shengshi_data/new_anti_tail/5classes.names") as f:
+    with open(class_name_path) as f:
         for idx, line in enumerate(f.readlines()):
             class_name = line.strip()
             classes_dict[class_name] = idx
+    
     
     width, height, objects = xml_reader(filename)
 
     lines = []
     nplines=[]
+    ignore_box=True
+    if ignore_box:
+        imagepath = os.path.join(os.path.dirname(filename), os.path.splitext(os.path.basename(filename))[0] + '.jpg').replace("Annotations/", "JPEGImages/")
+        img = cv2.imread(imagepath)  # BGR
+
     for obj in objects:
         x, y, x2, y2 = obj['bbox']
         class_name = obj['name']
         if class_name not in classes_dict:
+            print(f"{class_name} is not in classes_dict")
+            #填充灰度图覆盖 忽略区域
+            if ignore_box:
+                mask_pts=np.array([[x,y],[x2,y],[x2,y2],[x,y2]])
+                cv2.fillPoly(img, [mask_pts], (114,114,114))
             continue
         label = classes_dict[class_name]
+        ##防尾随 check wh_thr and ar_thr
         #过滤帽子标签
-        if label == 3:
-            continue
-        #其它类别归为一类
-        if label > 1:
-            label=2
-        else:
-            minwh=min(x2-x,y2-y)
-            wh_thr =min(minwh,wh_thr)
-            print("wh_thr=",wh_thr)
-            ar_size=(x2-x)*(y2-y)
-            ar_size=max(((x2-x)/(y2-y)),((y2-y)/(x2-x)))
-            ar_thr=max(ar_thr,ar_size)
-            print("ar_thr=",ar_thr)
+        # if label == 3:
+        #     continue
+        # #其它类别归为一类
+        # if label > 1:
+        #     label=2
+        # else:
+        #     minwh=min(x2-x,y2-y)
+        #     wh_thr =min(minwh,wh_thr)
+        #     print("wh_thr=",wh_thr)
+        #     ar_size=(x2-x)*(y2-y)
+        #     ar_size=max(((x2-x)/(y2-y)),((y2-y)/(x2-x)))
+        #     ar_thr=max(ar_thr,ar_size)
+        #     print("ar_thr=",ar_thr)
+
         cx = (x2+x)*0.5 / width
         cy = (y2+y)*0.5 / height
         w = (x2-x)*1. / width
@@ -73,12 +86,15 @@ def voc2yolo(filename,wh_thr,ar_thr):
         lines.append(line)
         nplines.append([label, cx, cy, w, h])
 
-    # if len(lines)>0 and (np.array(nplines)[:,1:]<=1).all():
-    #     txt_name = filename.replace(".xml", ".txt").replace("Annotations/", "txt/")
-    #     check_dir(os.path.dirname(txt_name))
-    #     with open(txt_name, "w") as f:
-    #         f.writelines(lines)
-
+    if len(lines)>0 and (np.array(nplines)[:,1:]<=1).all():
+        txt_name = filename.replace(".xml", ".txt").replace("Annotations/", "txt/")
+        check_dir(os.path.dirname(txt_name))
+        with open(txt_name, "w") as f:
+            f.writelines(lines)
+    if ignore_box:
+        save_roiimage_path=imagepath.replace("JPEGImages/", "txt_image/")
+        check_dir(os.path.dirname(save_roiimage_path))
+        cv2.imwrite(save_roiimage_path, img)
     #     imagepath = os.path.join(os.path.dirname(filename), os.path.basename(filename)[:-(4)] + '.jpg').replace("Annotations/", "JPEGImages/")
     #     save_roiimage_path=imagepath.replace("JPEGImages/", "txt_image/")
     #     check_dir(os.path.dirname(save_roiimage_path))
@@ -118,14 +134,16 @@ def imglist2file(imglist):
 
 
 if __name__ == "__main__":
-    xml_path_list = glob.glob("/home/lishuang/Disk/shengshi_data/new_anti_tail/Annotations/*.xml")
+    xml_path_list = glob.glob("/home/lishuang/Disk/shengshi_data/xilixiang/xingli/Annotations/*.xml")
+    #yolo
+    class_name_path="/home/lishuang/Disk/shengshi_data/xilixiang/xingli/classes.names"
     wh_thr=416
     ar_thr=1
     num=0
     for xml_path in xml_path_list:
         num=num+1
         print("num=",num)
-        wh_thr,ar_thr=voc2yolo(xml_path,wh_thr,ar_thr)
+        wh_thr,ar_thr=voc2yolo(xml_path,class_name_path,wh_thr,ar_thr)
 
 
     # imglist = get_image_list("/home/lishuang/Disk/shengshi_data/Xray/xianchang/images/train/2020060201")
